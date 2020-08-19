d8eefc80fa8e2bc131dac433cac5d6f64f8ee3f4
Caplan <rbiac013@BKUSL-1N7AP3XY.local>
2020-08-19 13:57:05 -0400

    sanity_queries

--- public/products.js
+++ public/products.js
@@ -1,10 +1,98 @@
+const cart = {}
+let products = [];
+let sanityData = {}
+
+const sanityUrl = 'https://6buksnvq.api.sanity.io/v1/graphql/production/default'
+const sanityQuery =`
+  query {
+   allCategory{
+      name 
+     primaryImage{
+       asset{
+         url
+       }
+     }
+     carouselImage{
+       asset{
+         url
+       }
+     }
+   }
+  }`
+
 const productsSection = document.querySelector('#products')
+const categoriesSection = document.querySelector('#categories');
 const cartDialog = document.querySelector('#dialog')
 const cartButton = document.querySelector('#cart-button')
 const lineItemsList = document.querySelector('#line-items')
-  
-const cart = {}
-let products = [];
+const logo = document.querySelector('#logo')
+const bagIcon = document.querySelector('#bag-icon')
+const backtoMainMenu = document.querySelector('#back-to-main-menu')
+
+//so data comes back from API as an object//
+async function fetchSanityData(){
+  const response = await fetch(sanityUrl, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({ query: sanityQuery})
+  })
+  const { data } = await response.json()
+  console.log(data)
+  sanityData = data
+}
+
+async function initialPageLoad(){
+  await fetchSanityData()
+//   // bagIcon.src = sanityData.allSiteSetting[0].bagIcon.asset.url
+//   bagIcon.classList.remove('hidden')
+//   logo.src = sanityData.allSiteSetting[0].logo.asset.url
+//   logo.classList.remove('hidden')
+  const fragments = sanityData.allCategory.map(renderCategory)
+  categoriesSection.innerHTML = ''
+  categoriesSection.prepend(...fragments)
+}
+
+const renderCategory = category => {
+  const html = `
+    <button class="category" data-category="${category.name}">
+      <img 
+        src="${category.primaryImage.asset.url}" 
+        alt="${category.name}"
+      />
+      <h2>${category.name}</h2>
+    </button>`
+  const fragment = document.createRange().createContextualFragment(html)
+  const button = fragment.querySelector('button')
+  button.addEventListener('click', selectCategory)
+  return fragment
+}
+
+
+const selectCategory = event => {
+  categoriesSection.classList.add('carousel')
+  const theButtonThatGotClicked = event.currentTarget
+  const allCategories = document.querySelectorAll('.category')
+  allCategories.forEach(category => category.classList.remove('selected'))
+  theButtonThatGotClicked.classList.add('selected')
+  const selectedCategory = theButtonThatGotClicked.dataset.category
+  const filteredProducts = products.filter(
+    product => product.category === selectedCategory
+  )
+  const fragments = filteredProducts.map(renderProduct);
+  productsSection.innerHTML = '';
+  productsSection.prepend(...fragments);  
+}
+
+const unselectCategory = () => {
+  categoriesSection.classList.remove('carousel')
+  const allCategories = document.querySelectorAll('.category')
+  allCategories.forEach(category => category.classList.remove('selected'))
+  productsSection.innerHTML = '';
+}
+
 
 function addToCart(event){
   const theButtonThatGotClicked = event.currentTarget
@@ -27,7 +115,7 @@ function subtractFromCart(event){
   }
   renderCart()
 }
-  
+
 const renderLineItem = priceId => {
   const product = products.find(prod => prod.price_id === priceId)
   const html = `
@@ -58,7 +146,6 @@ const toggleCart = () => {
   cartDialog.open = !cartDialog.open
 }  
 
-
 renderProduct = product => {
   const html = `
     <div class = "product" data-priceid="${product.price_id}">
@@ -71,20 +158,17 @@ renderProduct = product => {
     </div>`;
   const fragment = document.createRange().createContextualFragment(html);
   const button = fragment.querySelector('button');
-  button.addEventListener('click', addToCart);
+  button.addEventListener('click', selectCategory);
   return fragment;
 }
-
 
 async function fetchProducts(){
   const response = await fetch('/products');
   products = await response.json();
-  const fragments = products.map(renderProduct);
-  productsSection.innerHTML = '';
-  productsSection.prepend(...fragments)
 }
 
-
 cartButton.addEventListener('click', toggleCart)
+backtoMainMenu.addEventListener('click', unselectCategory)
 
+initialPageLoad()
 fetchProducts();
